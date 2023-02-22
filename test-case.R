97e20b4356bf9a613d9d2602ad8b2c0c716954cc
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-02-22 15:12:28 +0100

    Create a test case

---
+++ test-case.R
@@ -0,0 +1,6 @@
+source("naive-dt-fix.R")
+
+df <- data.frame(deelwoord = c("ingeplant", "gebeurt", "gebeurt", "gebeurd",
+                               "gebeurd", "gebeurd", "gebeurd", "verandert",
+                               "verandert", "veranderd", "veranderd",
+                               "veranderd", "geracet", "geraced", "geraced"))
