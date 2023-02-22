5dd257a0316f901cb77fa32e7badb36ecce6c710
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-02-22 15:12:52 +0100

    Finish test case

--- test-case.R
+++ test-case.R
@@ -4,3 +4,7 @@ df <- data.frame(deelwoord = c("ingeplant", "gebeurt", "gebeurt", "gebeurd",
                                "gebeurd", "gebeurd", "gebeurd", "verandert",
                                "verandert", "veranderd", "veranderd",
                                "veranderd", "geracet", "geraced", "geraced"))
+
+df <- fix_participle_dt(df, "deelwoord")
+
+print(df)
